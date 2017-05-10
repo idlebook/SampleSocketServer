d3f6c6d5e51d0cb38dd14e49ffe9ae7493a08070
tianshu wei <idlebook@163.cojm>
2017-05-10 19:43:52 +0800

    初始化工程

---
+++ SampleSocketServer/SampleSocketServer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SampleSocketServer
+//
+//  Created by tianshu wei on 2017/5/10.
+//  Copyright © 2017年 idlebook. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SampleSocketServer/SampleSocketServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleSocketServer
+//
+//  Created by tianshu wei on 2017/5/10.
+//  Copyright © 2017年 idlebook. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
